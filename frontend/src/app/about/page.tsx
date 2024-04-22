eedfb8254c1a850f30e381bad8e974ef420b85af
Baslael Workineh <baslaworku@gmail.com>
2024-04-22 14:16:59 +0300

    new folder structure

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div>
+      THIS IS ABOUT PAGE
+    </div>
+  )
+}
+
+export default about
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div>
-      this is about page
+      Hello world
     </div>
   )
 }
